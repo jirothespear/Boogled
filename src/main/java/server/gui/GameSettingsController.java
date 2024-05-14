4852fd2ed041a2539ee665a9015834462998c347
Renuel Balogo <2230174@slu.edu.ph>
2024-05-15 01:47:23 +0800

    Initial basis for the program implementation of the game settings

--- src/main/java/server/gui/GameSettingsController.java
+++ src/main/java/server/gui/GameSettingsController.java
@@ -1,5 +1,6 @@
 package server.gui;
 
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +8,19 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
 import java.io.IOException;
 
 public class GameSettingsController {
+
+    @FXML
+    private Spinner<Integer> roundTimeValue;
+
+    @FXML
+    private Spinner<Integer> waitingTimeValue;
+
     @FXML
     private Stage stage;
     @FXML
@@ -19,12 +29,32 @@ public class GameSettingsController {
     private Parent root;
 
     @FXML
+    public void initialize() {
+        roundTimeValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(Integer.MIN_VALUE, Integer.MAX_VALUE, 5));
+        waitingTimeValue.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(Integer.MIN_VALUE, Integer.MAX_VALUE, 3));
+
+        roundTimeValue.valueProperty().addListener(this::onRoundTimeChanged);
+        waitingTimeValue.valueProperty().addListener(this::onWaitingTimeChanged);
+    }
+
+    @FXML
+    public void onRoundTimeChanged(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+        System.out.println("Round Time changed from " + oldValue + " to " + newValue);
+        // Add your logic here for handling the new value
+    }
+
+    @FXML
+    public void onWaitingTimeChanged(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
+        System.out.println("Waiting Time changed from " + oldValue + " to " + newValue);
+        // Add your logic here for handling the new value
+    }
+
+    @FXML
     public void onBackButtonClick(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("server-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
 }
