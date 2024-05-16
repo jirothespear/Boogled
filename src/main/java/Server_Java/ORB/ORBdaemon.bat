8e1c395293ddeba14f8ddf357a02d708ebbda0d7
Liam Morales <dredknives04@gmail.com>
2024-05-16 22:41:32 +0800

    Fixed verbose.

--- src/main/java/Server_Java/ORB/ORBdaemon.bat
+++ src/main/java/Server_Java/ORB/ORBdaemon.bat
@@ -1,2 +1,1 @@
-@echo off
-start orbd -J-verbose -J-Djava.net.preferIPv4Stack=true -J-Djava.net.preferIPv6Addresses=false -ORBInitialPort 2055 -ORBInitialHost 192.168.56.1
+orbd  -ORBInitialPort 2055 -ORBInitialHost 192.168.56.1
