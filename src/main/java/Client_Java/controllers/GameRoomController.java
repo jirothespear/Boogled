5f5d367126f2ef96c037b623511a9261f889573c
Neil <neilcayton03@gmail.com>
2024-05-22 21:32:39 +0800

    After Game Dispose

--- src/main/java/Client_Java/controllers/GameRoomController.java
+++ src/main/java/Client_Java/controllers/GameRoomController.java
@@ -200,8 +200,9 @@ public class GameRoomController {
     public void onWindowCloseRequest() {
         Platform.runLater(() -> {
             System.out.println("Window is closing");
-            serverUtility.leaveGame(gameID,currentGameUser);
-            onGameFinished();
+//            serverUtility.leaveGame(gameID,currentGameUser);
+//            onGameFinished();
+            System.exit(0);
         });
 
     }
@@ -371,7 +372,7 @@ public class GameRoomController {
                         navigateToLobbyController();
                     }
                 }, 8000);
-
+                serverUtility.leaveGame(gameID,currentGameUser);
                 Stage gameRoomStage = (Stage) roundLabel.getScene().getWindow();
                 gameRoomStage.close();
             } catch (IOException e) {
